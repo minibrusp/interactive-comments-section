43c47676a25a03eeb6eda28825a88e8321cb056e
XIII <minibrusp@gmail.com>
2023-05-03 00:40:05 +0800

    Added Loading spinner on first load

--- frontend/src/components/CommentContainer.jsx
+++ frontend/src/components/CommentContainer.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useCommentContext from "../hooks/useCommentContext";
 
 import Comment from "./Comment";
@@ -7,17 +7,21 @@ import RepliesContainer from "./RepliesContainer";
 
 const CommentContainer = () => {
   const { comments, dispatch } = useCommentContext()
+  const [ isLoading, setIsLoading ] = useState(false)
 
   
   useEffect(() => {
     const fetchComments = async () => {
       try {
+        setIsLoading(true)
         const response = await fetch('http://localhost:3000/comments')
         const json = await response.json()
         if(response.ok) {
+          setIsLoading(false)
           dispatch({ type: 'SET_COMMENTS', payload: json})
         }
       } catch(err) {
+        setIsLoading(false)
         throw Error("Could not connect to the API")
       }
 
@@ -32,7 +36,7 @@ const CommentContainer = () => {
 
   return (
     <div className="comment__container m-4 mb-4 relative overflow-y-hidden max-w-[733px] mx-auto">
-      { comments && 
+      { comments &&
           comments.map((comment) => (
             <div key={comment.id}>
               <Comment 
@@ -48,6 +52,27 @@ const CommentContainer = () => {
               <RepliesContainer commentThreadId={comment.id} key={comment.content} replies={comment.replies}/>
             </div>
           ))
+      }
+
+      {
+        isLoading && (
+          <div className="comment__container__loader block text-center mx-auto">
+            <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-primary-moderate-blue motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+
+          </div>
+        )
+      }
+
+      {
+        (!comments && !isLoading) && (
+          <div className="comment__container__loader block text-center mx-auto my-4">
+            <span>no comments to show</span>
+          </div>
+        )
       }
       
     </div>
