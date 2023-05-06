0b930fe10d7620b202644a0fedcf864ac36a896b
XIII <minibrusp@gmail.com>
2023-05-07 03:49:01 +0800

    - added vercel.json - change comment and replies image source

--- frontend/src/components/CommentHeading.jsx
+++ frontend/src/components/CommentHeading.jsx
@@ -1,26 +1,12 @@
 /* eslint-disable react/prop-types */
 
-// avatars
-import avatarAmy from '../assets/images/avatars/image-amyrobson.webp'
-import avatarJulius from '../assets/images/avatars/image-juliusomo.webp'
-import avatarMax from '../assets/images/avatars/image-maxblagun.webp'
-import avatarRam from '../assets/images/avatars/image-ramsesmiron.webp'
-
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
-
-
-const avatars = new Map([
-  ['amyrobson', avatarAmy],
-  ['juliusomo', avatarJulius],
-  ['maxblagun', avatarMax],
-  ['ramsesmiron', avatarRam],
-])
-
 export default function CommentHeading({ user, isCurrent, createdAt}) {
+
   return (
     <div className="comment__heading flex justify-start items-center gap-3 mb-4 grid-in-header">
-      <img className='comment__avatar h-8 w-8 font-medium' src={avatars.get(`${user?.username}`)} alt="user image avatar" />
+      <img className='comment__avatar h-8 w-8 font-medium' src={user?.avatar} alt="user image avatar" />
       <p className='comment__username text-neutral-dark-blue'><strong className="font-medium text-[1.0125rem]">{user?.username}</strong></p>
       { 
         isCurrent && (
